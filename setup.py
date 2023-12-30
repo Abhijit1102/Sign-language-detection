85f9b024442e0b984f1ccab0b692108ec166a886
Abhijit1102 <abhijitrajkumar2@gmail.com>
2023-12-30 18:58:49 +0530

    30/12/23

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLang',
+    version= '0.0.0',
+    author= 'Abhijit',
+    author_email= 'abhijitrajkumar2@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
